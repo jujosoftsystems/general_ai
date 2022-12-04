da39c75c22ba60faa8b21d24f9bbb18ff81655db
RobCM <robert.coleman.martinez@gmail.com>
2022-12-04 16:35:34 -0600

    working on random generater code.

--- random_quotes.js
+++ random_quotes.js
@@ -7,7 +7,9 @@ function  random_quote() {
     const api_id = 2;
     let result = '';
 
-
+    // Generate random seed
+    let rand_quote_num = Math.floor(Math.random() * 20 + 1);
+    result = rand_quote_num;
 
     // Build array for JSON response
     let random_quote_result = [{
